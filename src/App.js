dac0a17aa6c2aed4cf07974fc6cb38b8d73f66d1
Ankit Verma <ankitverma209171@gmail.com>
2022-07-06 10:15:57 +0530

    react-router added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <nav>This is dummy nav</nav>
+            <Routes>
+                {/* <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} /> */}
+            </Routes>
+            <footer>This is dummy footer</footer>
+        </div>
+    );
 }
 
 export default App;
